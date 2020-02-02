af5efcd66f5b07558b5dce85c437075c46a2750e
Pinhas <arielpinhas@gmail.com>
2020-02-02 14:44:25 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
+// Import part
 import { Component } from '@angular/core';
 
+
+// Component decprator
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html',  // component Template file location
+  styleUrls: ['./app.component.scss']   // Style Sheet file location
 })
 
+
+// Logic
 export class AppComponent {
   title = 'MyApp1';
 }
